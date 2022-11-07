89a4aeda000a68246c91e8d2c042a0e3b32d07da
Mohammad Hadi Hosseinpour <wpparsi@gmail.com>
2022-11-08 03:11:37 +0330

    Fixes yamux configuration not having defaults set

--- yamux/config.go
+++ yamux/config.go
@@ -7,9 +7,10 @@ import (
 
 // getConfig creates a new instance of Config, with prefilled values.
 func getConfig(params muxedsocket.CommonParams) *S.Config {
-	return &S.Config{
-		EnableKeepAlive:    params.KeepalivePeriod <= 0 || params.MaxIdleTimeout <= params.KeepalivePeriod,
-		KeepAliveInterval:  params.KeepalivePeriod,
-		StreamCloseTimeout: params.MaxIdleTimeout,
-	}
+	config := S.DefaultConfig()
+	config.ConnectionWriteTimeout = params.MaxIdleTimeout
+	config.EnableKeepAlive = params.KeepalivePeriod <= 0 || params.MaxIdleTimeout <= params.KeepalivePeriod
+	config.KeepAliveInterval = params.KeepalivePeriod
+	config.StreamCloseTimeout = params.MaxIdleTimeout
+	return config
 }
