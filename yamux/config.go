1c230ef8656d4c7560ead5483ac5d92ec0eae159
Mohammad Hadi Hosseinpour <wpparsi@gmail.com>
2022-11-08 03:30:48 +0330

    Adds io.Discard as yamux logger

--- yamux/config.go
+++ yamux/config.go
@@ -3,11 +3,14 @@ package yamux
 import (
 	"github.com/hadi77ir/muxedsocket"
 	S "github.com/hashicorp/yamux"
+	"io"
 )
 
 // getConfig creates a new instance of Config, with prefilled values.
 func getConfig(params muxedsocket.CommonParams) *S.Config {
 	config := S.DefaultConfig()
+	// discard logs
+	config.LogOutput = io.Discard
 	config.ConnectionWriteTimeout = params.MaxIdleTimeout
 	config.EnableKeepAlive = params.KeepalivePeriod >= 0 && params.MaxIdleTimeout >= params.KeepalivePeriod
 	config.KeepAliveInterval = params.KeepalivePeriod
